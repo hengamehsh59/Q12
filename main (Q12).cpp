813a39522c4250aaae306a06512b827f703fa4a8
hengamehsh59 <hengamehsh59@gmail.com>
2024-06-08 18:42:54 +0330

    Add files via upload

---
+++ main (Q12).cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cin>>a;
+int modA=0;
+for(int i=2;i<a/2;i++)
+{
+    if(a%i==0);
+    {
+        modA++;
+        if(modA==0);
+        {
+            cout<<"It is primary";
+        }
+        return 0;
+    }
+}
+}
